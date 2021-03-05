c92b0d59847ad1621f96e665f752e050c677173f
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-03-04 21:59:00 -0800

    api calls

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const apiData = require('./routes/api');
 
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+apiData(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
   });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,28 @@
+const tableData = require('../data/tableData');
+const waitingListData = require('../data/waitingListData');
 
+function apiData(app) {
 
+app.get('/api/tableData', (req, res) => res.json(tableData));
+app.get('/api/waitingListData', (req, res) => res.json(waitingListData));
 
+app.post('/api/tableData', (req, res) => {
+    if (tableData.length < 5) {
+        tableData.push(req.body);
+        res.json(true);
+      } else {
+        waitingListData.push(req.body);
+        res.json(false);
+      }
+});
 
+app.post('/api/clear', (req, res) => {
+    tableData.length = 0;
+    waitListData.length = 0;
 
+    res.json({ ok: true });
+  });
 
+};
 
-
-
-module.exports
+module.exports = apiData;
